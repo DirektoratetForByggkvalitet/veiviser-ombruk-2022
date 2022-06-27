40c6bb2cb448e291f2a89c5115d77079472e2c5a
hprestmo <43567228+hprestmo@users.noreply.github.com>
2022-06-27 14:53:59 +0200

    Structural changes

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -20,19 +20,15 @@ function Intro({ close, data: { $computed, ...data } }) {
         Før du begynner er det nyttig at du vet: 
         <ul>
           <li>hvor gammel byggevaren er</li>
-          <li>om byggevaren har stått i et norsk byggverk eller kommer fra utlandet</li>
           <li>om du har produktdokumentasjon, sertifisering eller godkjenning eller annen dokumentasjon fra da byggevaren først ble omsatt (FDV-dokumentasjon kan si noe om opprinnelsen til produktet)</li>
           <li>andre opplysninger om behandlingen av byggevaren fra rivingsprosessen</li>
         </ul>
         Før byggevarer blir demontert fra et eksisterende byggverk skal det gjennomføres en kartlegging av farlig avfall og bygningsfraksjoner som må fjernes. Byggevarer som er farlig avfall, kan ikke ombrukes, men skal leveres som farlig avfall til godkjent mottak.
-        <h2>Skillet mellom omsetning og bruk av byggevaren</h2> 
-        Det er forskjell på kravene til omsetning og salg av byggevarene og kravene til bruk av byggevarer i byggverk. 
+        <h2>Nyttige lenker for deg som ønsker å ombruk en byggevare</h2>  
         <ul> 
-          <li>Kravene til omsetning og salg står i byggevareforskriften. De retter seg mot produsenter, importører og distributører av byggevarer. DiBK fører tilsyn med produktene.</li>
-          <li>Kravene til bruk av byggevarer i byggverk er beskrevet i byggteknisk forskrift. De retter seg mot det ansvarlige foretaket i byggesaken. Kommunen fører tilsyn med byggesaken.</li>
+          <li> <a href='https://vcob.dk/vcob/cirkulaert-byggeri/byggevarer/materialeatlas/' target='_blank'>"Materialatlas"</a> laget av det danske Videncenter for Cirkulær Økonomi i Byggeriet</li>
+          <li> <a href='https://www.sintefbok.no/book/index/1320/ombruk_av_byggematerialer_veileder_for_dokumentasjon_av_ytelser' target='_blank'>Veileder for dokumentasjon av ytelser ved ombruk av byggematerialer</a> av SINTEF Community</li>
         </ul>
-        <p><img src="https://dibk.no/globalassets/produkter/omsetning_bruk.svg"/></p>
-        <p>I denne veiviseren er fokuset på kravene til omsetning/salg av brukte byggevarer.</p>
         
 
         </Primitives.Paragraphs.Lead>
